56d1e1392889ba3d639faf1f37b38fdca16fd3c0
JuniimSI <jrfernandesti@gmail.com>
2018-07-25 20:33:42 -0300

    PARTE 3 - 1

---
+++ backend/app/Controllers/Http/ImageController.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Image = use('App/Models/Image')
+const Property = use('App/Models/Property')
+
+/**
+ * Resourceful controller for interacting with images
+ */
+class ImageController {
+  /**
+   * Create/save a new image.
+   * POST images
+   */
+  async store ({ request }) {
+    
+  }
+}
+
+module.exports = ImageController
